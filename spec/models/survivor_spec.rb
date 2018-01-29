af5aab8ebee4a5b2e40dd4396c74037e014a016a
Rogerio Bispo <rogerio_pd@yahoo.com.br>
2018-01-29 21:55:31 -0200

    test to survivor presence of fields

--- spec/models/survivor_spec.rb
+++ spec/models/survivor_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Survivor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should validate presence' do
+    survivor = Survivor.new
+    survivor.name = ''
+    survivor.gender = ''
+    survivor.valid?
+    survivor.errors[:name].should include("can't be blank")
+
+    survivor.name = 'Rogerio Bispo'
+    survivor.age = 34
+    survivor.gender = 'M'
+    survivor.last_longitude = '46'
+    survivor.last_latitude = '46'
+    survivor.valid?
+    survivor.errors[:name].should_not include("can't be blank")
+  end
 end
